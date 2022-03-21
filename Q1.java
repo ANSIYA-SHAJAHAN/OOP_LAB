b5ef8f20b0faddf0c773835bc8cdd0d6069dc2a4
ANSIYA-SHAJAHAN <95459632+ANSIYA-SHAJAHAN@users.noreply.github.com>
2022-03-21 10:31:50 +0530

    Add files via upload

---
+++ Q1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Q1 {
+ public static void main(String[] args)
+ {  
+int rollno,mark;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your name");
+String str=sc.nextLine();
+System.out.println("Enter your rollno:");
+rollno = sc.nextInt();
+System.out.println("Enter your Mark:");
+mark = sc.nextInt();
+
+
+
+
+System.out.println("The Name is:" +str);
+System.out.println("The Rollno is:" +rollno);
+System.out.println("The Mark is:" +mark);
+
+ }
+}
+
+
